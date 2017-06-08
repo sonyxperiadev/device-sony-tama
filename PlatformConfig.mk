020517ba548c6b25417fab872064d35bf0cf3cbe
Thierry Strudel <tstrudel@google.com>
2017-06-07 23:34:46 -0700

    PRODUCT_SHIPPING_API_LEVEL := 26
    Bug: 62434384
    Change-Id: I3e254413356460df77d5d9c5da2f13b4e9bda243
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -95,7 +95,7 @@ TARGET_USES_SDE := true
 
 # Treble
 BOARD_VNDK_VERSION := current
-PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_SHIPPING_API_LEVEL := 26
 
 # Build a separate vendor.img
 TARGET_COPY_OUT_VENDOR := vendor
