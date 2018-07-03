06c6d4db48cc1600a1e49fb93b11fa58fa2b7fc2
Alin Jerpelea <alin.jerpelea@sony.com>
2018-07-03 13:23:31 +0200

    Update serial console
    The serial console setup was incorect and we had no output
    With the proper serial console setup we can now get logs from device
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -41,7 +41,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 
 # Serial console
-#BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.console=msm_serial_dm,0xc170000
+#BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xa84000 androidboot.console=msm_serial_dm,0xa84000
 
 TARGET_RECOVERY_WIPE := $(PLATFORM_COMMON_PATH)/rootdir/recovery.wipe
 TARGET_RECOVERY_FSTAB = $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.tama
