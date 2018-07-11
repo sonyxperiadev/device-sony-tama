7c896b05b774fe25b900a361e719d68ce2187d0b
Alin Jerpelea <alin.jerpelea@sony.com>
2018-07-11 10:28:59 +0200

    KERNEL_CMDLINE: enable service_locator
    the feature is needed by the platform and should be enabled
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -42,6 +42,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=1d84000.ufshc
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += msm_drm.dsi_display0=dsi_panel_cmd_display:config0
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
 
 # Serial console
 #BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xa84000 androidboot.console=msm_serial_dm,0xa84000
