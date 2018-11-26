eef23c8e06989cc2915b240f9d0aee3c4a3adb87
Alin Jerpelea <alin.jerpelea@sony.com>
2018-11-26 09:03:18 +0100

    sensors: drop deprecated config properties
    the legacy properties are not used on sdm845 platform
    and can be removed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -134,52 +134,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence
 
-# aDSP sensors
-## max rate
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qti.sensors.max_accel_rate=false \
-    ro.qti.sensors.max_gyro_rate=false \
-    ro.qti.sensors.max_mag_rate=false \
-    ro.qti.sensors.max_geomag_rotv=50
-
-## sensor type
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qti.sdk.sensors.gestures=false \
-    ro.qti.sensors.pedometer=false \
-    ro.qti.sensors.step_detector=true \
-    ro.qti.sensors.step_counter=true \
-    ro.qti.sensors.pam=false \
-    ro.qti.sensors.scrn_ortn=false \
-    ro.qti.sensors.smd=true \
-    ro.qti.sensors.game_rv=true \
-    ro.qti.sensors.georv=true \
-    ro.qti.sensors.cmc=false \
-    ro.qti.sensors.bte=false \
-    ro.qti.sensors.fns=false \
-    ro.qti.sensors.qmd=false \
-    ro.qti.sensors.amd=false \
-    ro.qti.sensors.rmd=false \
-    ro.qti.sensors.vmd=false \
-    ro.qti.sensors.gtap=false \
-    ro.qti.sensors.tap=false \
-    ro.qti.sensors.facing=false \
-    ro.qti.sensors.tilt=false \
-    ro.qti.sensors.tilt_detector=true \
-    ro.qti.sensors.dpc=false \
-    ro.qti.sensors.qheart=false \
-    ro.qti.sensors.wu=true \
-    ro.qti.sensors.proximity=true \
-    ro.qti.sensors.gravity=true \
-    ro.qti.sensors.laccel=true \
-    ro.qti.sensors.orientation=true \
-    ro.qti.sensors.rotvec=true \
-    ro.qti.sensors.fast_amd=false \
-    ro.qti.sensors.wrist_tilt=false \
-    ro.qti.sensors.pug=false \
-    ro.qti.sensors.iod=false \
-    ro.qti.sensors.als_scale=1000 \
-    ro.qfusion_use_report_period=false
-
 # USB controller setup
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.controller=a600000.dwc3 \
