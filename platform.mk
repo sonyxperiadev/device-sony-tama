8890a97dbbc16643d2d647f6a871f334107a3263
Marijn Suijten <marijns95@gmail.com>
2020-06-26 14:44:11 +0200

    platform: Create a separate policy for the CAF and AOSP audio HAL
    Creating a shared policy base between AOSP and CAF is almost impossible
    as we try to override and update more and more nodes such as extending
    existing mixPorts. While moving such nodes into a separate file for AOSP
    and CAF is definitely possible, the resulting set of shared nodes is
    relatively small for the extra management overhead it costs us to
    compare and split these out.
    
    Instead, duplicate the entire policy: freeze AOSP in place and create
    a copy for CAF. Note that this temporarily does away with the PARTIAL
    CAF policy files in favour of importing them all at once in a clean
    followup commit That policy was anyway rejected by APM, see early pushes
    to https://github.com/sonyxperiadev/device-sony-kumano/pull/38.
    With the changes in a separate commit the total diff from AOSP to CAF is
    much easier to review.
    
    In the end the AOSP policy persists only for future reference and as
    fallback, but is likely to be removed regardless; the CAF audio HAL is
    by far superior. Testing and maintaining this policy is overhead.
    
    Signed-off-by: Marijn Suijten <marijns95@gmail.com>
    
    MAEND

--- platform.mk
+++ platform.mk
@@ -76,8 +76,12 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
     $(SONY_ROOT)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(SONY_ROOT)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(SONY_ROOT)/vendor/etc/audio_policy_configuration_bluetooth_legacy_hal.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml \
-    $(SONY_ROOT)/vendor/etc/common_primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/common_primary_audio_policy_configuration.xml
+    $(SONY_ROOT)/vendor/etc/audio_policy_configuration_bluetooth_legacy_hal.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_bluetooth_legacy_hal.xml
+
+# Audio - Separation between plain AOSP configuration and extended CodeAurora Audio HAL features
+AUDIO_HAL_TYPE := $(if $(filter true,$(TARGET_USES_AOSP_AUDIO_HAL)),aosp,caf)
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/$(AUDIO_HAL_TYPE)_common_primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/common_primary_audio_policy_configuration.xml
 
 # Media
 PRODUCT_COPY_FILES += \
