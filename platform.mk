ede757534d10bf3b3da8d9c4736ac7ce71acaf5f
Alin Jerpelea <alin.jerpelea@sony.com>
2021-05-11 11:08:08 +0200

    platform: remove deprecated display packages barrier
    Those packages were used by legacy devices and can be removed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -80,9 +80,6 @@ TARGET_USES_SDE := true
 DEVICE_PACKAGE_OVERLAYS += \
     $(PLATFORM_COMMON_PATH)/overlay
 
-# Graphics allocator/mapper v3
-TARGET_HARDWARE_GRAPHICS_V3 := true
-
 # A/B support
 AB_OTA_UPDATER := true
 PRODUCT_SHIPPING_API_LEVEL := 26
